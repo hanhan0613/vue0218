dc669ef122f16794ae9d395cece2267118ada8d7
hanhan0613 <17639935682@163.com>
2025-02-28 19:19:28 +0800

    Router0228

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,27 @@
 //创建一个路由器,并暴露出去
 
 //第一步:引入createRouter
-import { createRouter,createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 //引入组件
-import Home from '@/components/Home.vue'
-import News from '@/components/News.vue'
-import About from '@/components/About.vue'
+import Home from '@/pages/Home.vue'
+import News from '@/pages/News.vue'
+import About from '@/pages/About.vue'
 
 //第二步:创建路由器
 const router = createRouter({
-    history:createWebHistory(),//路由器的工作模式
-    routes:[
+    history: createWebHistory(),//路由器的工作模式
+    routes: [
         {
-            path:'/home',
-            component:Home
+            path: '/home',
+            component: Home
         },
         {
-            path:'/news',
-            component:News
+            path: '/news',
+            component: News
         },
         {
-            path:'/about',
-            component:About
+            path: '/about',
+            component: About
         }
     ]
 })
